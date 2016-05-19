bd2660ab0572720a5641b50ed0ea247dbe38385a
StureTh <sturethuren@gmail.com>
2016-05-19 14:18:14 +0200

    Fixed some stuff

--- Lab01HTML/FlygstolenDemo/scripts/PrintDate.js
+++ Lab01HTML/FlygstolenDemo/scripts/PrintDate.js
@@ -1,8 +1,8 @@
 ﻿function printdate() {
     var dd = new Date().getDate();
     var mm = new Date().getMonth() + 1;
-    var yyy = new Date().getFullYear();
+    var yyyy = new Date().getFullYear();
 
-    document.getElementById('writedate').value = yyy + '-0' + mm + '-' + dd;
+    document.getElementById('writedate').value = yyyy + '-0' + mm + '-' + dd;
   
 }
