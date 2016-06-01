f89da5a85ccb0d76308a8aee80fa8cdbdf318fc2
StureTh <sturethuren@gmail.com>
2016-06-01 09:58:35 +0200

    Made a canvas demo and added section, fixed postion of aside on lab4

---
+++ Lab01HTML/CanvasDemo/scripts/drawcanvas.js
@@ -0,0 +1,14 @@
+﻿alert("Hej")
+
+$(document).ready(function () {
+    draw();
+    console.log("ready!");
+});
+
+function draw() {
+    var canvas = document.getElementById('paint');
+    var ctx = canvas.getContext('2d');
+    ctx.fillRect(10, 20, 30, 200);
+    ctx.fillRect(80, 20, 30, 200);
+    ctx.fillRect(40,75,70,20)
+}
